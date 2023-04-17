d8d9ba61f70a732295893881deccceaf259106cd
Dugi <dugisurdulli@gmail.com>
2023-04-17 14:57:43 -0600

    emit types in dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		dts({
+			insertTypesEntry: true
+		})
+	],
 	test: {
 		globals: false,
 		css: true,
